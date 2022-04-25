350cc72514c2c22e4ee4213fb4fd0d70cab874e9
Bruno Alencar <joaobrunoalencar@gmail.com>
2022-04-25 00:42:51 -0300

    refactor: :recycle: refactor name of function

--- code_refactoring/test/example2/cpf.test.ts
+++ code_refactoring/test/example2/cpf.test.ts
@@ -1,73 +1,73 @@
-import { validate } from '../../src/example2/cpf';
+import { isValidCPF } from '../../src/example2/cpf';
 
 test('should return cpf is valid', () => {
-  const result = validate('108.908.334-35');
+  const result = isValidCPF('108.908.334-35');
 
   expect(result).toBe(true);
 });
 
 test('should return cpf is valid', () => {
-  const result = validate('111.444.777-35');
+  const result = isValidCPF('111.444.777-35');
 
   expect(result).toBe(true);
 });
 
 test('should return cpf is invalid', () => {
-  const result = validate('111.444.777-33');
+  const result = isValidCPF('111.444.777-33');
 
   expect(result).toBe(false);
 });
 
 test('should return cpf is invalid', () => {
-  const result = validate('111.444.777-333');
+  const result = isValidCPF('111.444.777-333');
 
   expect(result).toBe(false);
 });
 
 test('should return cpf is invalid', () => {
-  const result = validate('111.444.777-333333');
+  const result = isValidCPF('111.444.777-333333');
 
   expect(result).toBe(false);
 });
 
 test('should return cpf is invalid', () => {
-  const result = validate('111.111.111-11');
+  const result = isValidCPF('111.111.111-11');
 
   expect(result).toBe(false);
 });
 
 test('should return cpf is invalid', () => {
-  const result = validate(null);
+  const result = isValidCPF(null);
 
   expect(result).toBe(false);
 });
 
 test('should return cpf is invalid', () => {
-  const result = validate(undefined);
+  const result = isValidCPF(undefined);
 
   expect(result).toBe(false);
 });
 
 test('should return cpf is invalid', () => {
-  const result = validate('319.956.164-00');
+  const result = isValidCPF('319.956.164-00');
 
   expect(result).toBe(true);
 });
 
 test('should return cpf is invalid', () => {
-  const result = validate('abc.dfe.ghi-jl');
+  const result = isValidCPF('abc.dfe.ghi-jl');
 
   expect(result).toBe(false);
 });
 
 test('should return cpf is invalid', () => {
-  expect(() => validate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])).toThrow(
-    new Error('str.replace is not a function')
+  expect(() => isValidCPF([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])).toThrow(
+    new Error('cpf.replace is not a function')
   );
 });
 
 test('should return cpf is invalid', () => {
-  const result = validate('295$379\n955...93');
+  const result = isValidCPF('295$379\n955...93');
 
   expect(result).toBe(false);
 });
